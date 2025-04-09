357be29f97dfd976e32f0fc8028d7416d9143287
Christian Nuss <christian.nuss@gmail.com>
2025-04-09 08:31:22 -0400

    initial commit

---
+++ dist/google/api/annotations_pb.js
@@ -0,0 +1,29 @@
+"use strict";
+// Copyright 2015 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.http = exports.file_google_api_annotations = void 0;
+var codegenv1_1 = require("@bufbuild/protobuf/codegenv1");
+var http_pb_1 = require("./http_pb");
+var wkt_1 = require("@bufbuild/protobuf/wkt");
+/**
+ * Describes the file google/api/annotations.proto.
+ */
+exports.file_google_api_annotations = (0, codegenv1_1.fileDesc)("Chxnb29nbGUvYXBpL2Fubm90YXRpb25zLnByb3RvEgpnb29nbGUuYXBpOksKBGh0dHASHi5nb29nbGUucHJvdG9idWYuTWV0aG9kT3B0aW9ucxiwyrwiIAEoCzIULmdvb2dsZS5hcGkuSHR0cFJ1bGVSBGh0dHBCbgoOY29tLmdvb2dsZS5hcGlCEEFubm90YXRpb25zUHJvdG9QAVpBZ29vZ2xlLmdvbGFuZy5vcmcvZ2VucHJvdG8vZ29vZ2xlYXBpcy9hcGkvYW5ub3RhdGlvbnM7YW5ub3RhdGlvbnOiAgRHQVBJYgZwcm90bzM", [http_pb_1.file_google_api_http, wkt_1.file_google_protobuf_descriptor]);
+/**
+ * See `HttpRule`.
+ *
+ * @generated from extension: google.api.HttpRule http = 72295728;
+ */
+exports.http = (0, codegenv1_1.extDesc)(exports.file_google_api_annotations, 0);
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export * from "./etcd/api/authpb/auth_pb";
+export * from "./etcd/api/etcdserverpb/etcdserver_pb";
+export * from "./etcd/api/etcdserverpb/rpc_pb";
+export * from "./etcd/api/mvccpb/kv_pb";
